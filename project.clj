73a132407867835cf11f28a463f4415c719fa277
Dave Parfitt <dparfitt@getchef.com>
2014-07-09 14:06:09 -0400

    initial add

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject storm_clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :aot :all
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main storm-clj.core 
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 ;[storm/storm-core "0.9.0.1"]
+                 ;[storm/storm-netty "0.9.0.1"]
+                 ;[storm/tools.cli "0.2.2"]
+                 [storm "0.9.0.1"]
+                 ])
